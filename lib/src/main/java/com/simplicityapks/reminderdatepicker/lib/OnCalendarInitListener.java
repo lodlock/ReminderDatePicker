edc801d0472f04f22b3c1eb3c3283fe5fdf978a7
Brandon Andrews <iamhunted@gmail.com>
2016-07-22 14:56:57 -0400

    Create OnCalendarInitListener.java

---
+++ lib/src/main/java/com/simplicityapks/reminderdatepicker/lib/OnCalendarInitListener.java
@@ -0,0 +1,12 @@
+package com.simplicityapks.reminderdatepicker.lib;
+
+import java.util.Calendar;
+
+/**
+ * Created by lodlock (iamhunted@gmail.com)
+ * On 4/18/2015 7:18 AM
+ * For the ReminderDatePicker (com.simplicityapks.reminderdatepicker.lib) project
+ */
+public interface OnCalendarInitListener {
+    public Calendar onCalendarInit();
+}
